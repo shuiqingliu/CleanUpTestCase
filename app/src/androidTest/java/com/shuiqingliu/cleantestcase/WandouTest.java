bc6a703c0fe71ae9e5c2d744b11ac4fb5cfeb50e
qingliu <shuiqingliu14@gmail.com>
2015-11-27 03:13:35 +0800

    修复一些逻辑问题,睡了一觉感觉不完善

--- app/src/androidTest/java/com/shuiqingliu/cleantestcase/WandouTest.java
+++ app/src/androidTest/java/com/shuiqingliu/cleantestcase/WandouTest.java
@@ -7,6 +7,7 @@ import android.support.test.uiautomator.UiAutomatorTestCase;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
 import org.junit.Test;
@@ -21,6 +22,7 @@ import org.junit.runner.RunWith;
 public class WandouTest extends UiAutomatorTestCase{
 
     private UiDevice mUiDevice;
+    private boolean isOK;
 
     @Test
     public void testDemo() throws UiObjectNotFoundException, RemoteException {
@@ -30,31 +32,32 @@ public class WandouTest extends UiAutomatorTestCase{
         UiObject Apps = new UiObject(new UiSelector().description("Apps"));
         Apps.click();
         //进入急速清理应用
+
+        UiScrollable listScrollable = new UiScrollable(new UiSelector().scrollable(true));
+        listScrollable.scrollDescriptionIntoView("极速清理");
         UiObject cleanSpeed = new UiObject(new UiSelector().text("极速清理"));
         cleanSpeed.clickAndWaitForNewWindow();
 
         UiObject actionButton = new UiObject(new UiSelector().resourceId("com.fastclean:id/action_button"));
         //判断清理按钮是否存在
-        if (actionButton.exists()){
-            actionButton.waitForExists(1000);
+        if (actionButton.waitForExists(20000)) {
             actionButton.click();
             getUiDevice().pressBack();
-            getUiDevice().getInstance().pressRecentApps();
-        }else {
+        }
             //当弹出添加桌面快捷方式的处理
             UiObject noAgree = new UiObject(new UiSelector().text("不用了"));
-            if (noAgree.exists()){
+            if (noAgree.exists()) {
                 noAgree.click();
                 getUiDevice().getInstance().pressHome();
-                getUiDevice().getInstance().pressRecentApps();
-
+                isOK = true;
             }
-        }
 
+        getUiDevice().getInstance().pressRecentApps();
         //结束到清理进程
         UiObject killCleanApp = new UiObject(new UiSelector().description("Dismiss 极速清理."));
-        killCleanApp.waitForExists(500);
-        killCleanApp.click();
-        getUiDevice().getInstance().pressHome();
+        if (killCleanApp.waitForExists(5000)){
+            killCleanApp.click();
+            getUiDevice().getInstance().pressHome();
+        }
     }
 }
